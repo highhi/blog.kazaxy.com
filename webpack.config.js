cdbf725430d7fe30a9e00419a66b88486412bab7
kazu <kazu8160@gmail.com>
2017-12-06 21:03:32 +0900

    開発用ディレクトリの追加。static配下を管理外に

--- webpack.config.js
+++ webpack.config.js
@@ -4,37 +4,37 @@ const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 const promisify = require('util').promisify;
 const execFile = require('child_process').execFile;
 
-const configPromise = new Promise((resolve, reject) => {
-  execFile('git', ['rev-parse', 'HEAD'], (_, stdout) => {
-    resolve({
-      entry: {
-        sw: path.resolve(__dirname, 'src', 'sw.js')
-      },
-  
-      output: {
-        path: path.resolve(__dirname, 'static'), filename: '[name].js'
-      },
-  
-      module: {
-        rules: [
-          // use bable
-          { test: /\.js?$/, use: 'babel-loader', exclude: /node_modeules/ },
-          // use typescript
-          // { test: /\.tsx?$/, use: 'awesome-typescript-loader', exclude: /node_modlues/ },
-        ],
-      },
-  
-      plugins: [
-        new webpack.DefinePlugin({
-          'process.env': {
-            NODE_ENV: JSON.stringify(process.env.NODE_ENV),
-            GIT_REVISION_HASH: JSON.stringify(stdout.trim()),
-          },
-        }),
-        //new UglifyJsPlugin()
-      ]
-    });
-  });
-});
+const IS_PROD = process.env.NODE_ENV === 'production';
+const plugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+      GIT_REVISION_HASH: JSON.stringify(process.env.GIT_REVISION_HASH),
+    },
+  }),
+];
 
-module.exports = () => configPromise;
+if (IS_PROD) {
+  plugins.push(new UglifyJsPlugin());
+}
+
+module.exports = {
+  entry: {
+    sw: path.resolve(__dirname, 'src', 'sw.js')
+  },
+
+  output: {
+    path: path.resolve(__dirname, 'static'), filename: '[name].js'
+  },
+
+  module: {
+    rules: [
+      // use bable
+      { test: /\.js?$/, use: 'babel-loader', exclude: /node_modeules/ },
+      // use typescript
+      // { test: /\.tsx?$/, use: 'awesome-typescript-loader', exclude: /node_modlues/ },
+    ],
+  },
+
+  plugins: plugins,
+}
